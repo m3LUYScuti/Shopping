a4bdb6d4815234667a164ce7d8dcc785562010e3
davit <davit@GOSH>
2024-09-25 10:23:56 +0400

    Add project files.

---
+++ Shopping/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Ra dagijda tqveni shenadzeni:");
+
+int Tanxa = int.Parse(Console.ReadLine());
+
+
+
+if (Tanxa <= 100 )
+{
+    Console.WriteLine($"Tqveni danaxarji ar eqvemdebareba fasdaklebas. Gadasaxdelia {Tanxa}");
+}
+else if (Tanxa >= 101 && Tanxa <= 500)
+{
+    Console.WriteLine($"Tqven gekutvniT 10% fasdakleba. Gadasaxdelia {Tanxa-(Tanxa/10)}  ");
+}
+else if (Tanxa >= 500)
+{
+    Console.WriteLine($"Tqven gekutvnit 20% fasdakleba. Gadasaxdelia { Tanxa - (Tanxa / 5)}");
+}
+
+else
+{
+    Console.WriteLine("Tqven sheiyvanet araswori kodi");
+}
+
+
+Console.ReadLine();
